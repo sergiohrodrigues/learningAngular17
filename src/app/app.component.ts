8561d0b007923b8313a92c8f7f2d7b18d34109a6
Sergio <sergiohrodriguess@gmail.com>
2025-02-06 11:10:22 -0300

    Learning to use the new @if and @for template syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,16 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'learningAngular17';
+
+  userDatasList: Array<{
+    name: string,
+    age: number,
+    profession: string,
+    id: string
+  }> = [
+    { age: 20, name: 'Marcos', profession: 'Software Developer', id: '123'},
+    { age: 30, name: 'Marcelo', profession: 'Software Developer', id: '456'},
+    { age: 40, name: 'Carlos', profession: 'Scrum Master', id: '789'},
+    { age: 30, name: 'Maria', profession: 'UX Designer', id: '123'},
+  ]
 }
