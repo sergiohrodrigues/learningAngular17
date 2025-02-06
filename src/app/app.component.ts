45fbfd78cc7ff8106d832a2d0fb9d357bab3bba2
Sergio <sergiohrodriguess@gmail.com>
2025-02-06 11:42:37 -0300

    learning to use new @switch template syntaxe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,29 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; grid-area: 1rem;">
+      @for (user of userDatasList; track $index) {
+        @switch(user.age){
+          @case (20) {
+            <span style="color: blue;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
+          }
+          @case (30) {
+            <span style="color: red;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
+          }
+          @case (40) {
+            <span style="color: brown;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
+          }
+          @default {
+            <span>{{ user.name }} is {{ user.profession }} and your age is {{ user.age }}</span>
+          }
+        }
+      } @empty {
+        <span style="color: red;">Don't have datas to show</span>
+      }
+    </div>
+  `,
+  styleUrls: []
 })
 export class AppComponent {
   title = 'learningAngular17';
