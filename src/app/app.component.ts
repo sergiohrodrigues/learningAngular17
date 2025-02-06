633d7f6bcc936f38948051a5f72cf740934d7e6c
Sergio <sergiohrodriguess@gmail.com>
2025-02-06 13:55:20 -0300

    learning to use the new @defer template syntax

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,18 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: `
     <div style="display: flex; justify-content: center; align-items: center; flex-direction: column; grid-area: 1rem;">
-      @for (user of userDatasList; track $index) {
-        @switch(user.age){
-          @case (20) {
-            <span style="color: blue;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
-          }
-          @case (30) {
-            <span style="color: red;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
-          }
-          @case (40) {
-            <span style="color: brown;">{{ user.name }} is {{ user.profession }} and is 20 years old</span>
-          }
-          @default {
-            <span>{{ user.name }} is {{ user.profession }} and your age is {{ user.age }}</span>
-          }
-        }
-      } @empty {
-        <span style="color: red;">Don't have datas to show</span>
+<button (click)="renderBlock = true">Renderizar título</button>
+      @defer (when renderBlock){
+        <h3 style="color: red;">Element rendered on interaction</h3>
+      } @placeholder {
+        <h3>Conteudo inicial do placeholder</h3>
       }
     </div>
   `,
   styleUrls: []
 })
 export class AppComponent {
+  renderBlock = false;
   title = 'learningAngular17';
 
   userDatasList: Array<{
