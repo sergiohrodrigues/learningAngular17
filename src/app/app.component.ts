4d9461bcdc69d03d4d16fa31717540b4beb474cc
Sergio <sergiohrodriguess@gmail.com>
2025-02-06 16:01:41 -0300

    Description of observables using takeUntilDestroyed()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InputTransformComponent } from "./components/input-transform.componet";
+import { of } from 'rxjs';
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 export interface User {
   name: string,
@@ -18,10 +20,12 @@ export interface User {
   `,
   styleUrls: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  destroyedRef = inject(DestroyRef);
   showUserAge = "true"
   renderBlock = false;
   title = 'learningAngular17';
+
 
   userDatasList: Array<User> = [
     { age: '20', name: 'Marcos', profession: 'Software Developer', id: '123'},
@@ -29,4 +33,16 @@ export class AppComponent {
     { age: '40', name: 'Carlos', profession: 'Scrum Master', id: '789'},
     { age: '30', name: 'Maria', profession: 'UX Designer', id: '123'},
   ]
+
+  userDatas$ = of(this.userDatasList);
+
+  ngOnInit(): void {
+    this.userDatas$
+    .pipe(takeUntilDestroyed(this.destroyedRef))
+    .subscribe({
+      next: (response) => {
+        console.log('User Datas in Observable', response)
+      }
+    })
+  }
 }
